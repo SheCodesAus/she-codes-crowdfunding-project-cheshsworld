1d9fa45befb5d3a526d2d633f5b910851a21ded0
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-04-19 20:31:27 +0800

    Started CrowdFund project

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Data
+import { allProjects } from "../data";
+
+function HomePage() {
+    return (
+        <div>
+            {allProjects.map((projectData, key) => {
+                return <div key={key}>{projectData.title}</div>;
+            })}
+        </div>
+    );
+}
+
+export default HomePage;
