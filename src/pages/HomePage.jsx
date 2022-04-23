3ac900743f06d38dfc7ab7361d38345941dc2c33
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-04-23 11:51:46 +0800

    added in gitignore and updated to load projects

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //Components
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
 //Data
-import { allProjects } from "../data";
+// import { allProjects } from "../data";
 
 function HomePage() {
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}projects`)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+        setProjectList(data)
+        });        ;
+    }, []);
+
     return (
         <div id="project-list">
-            {allProjects.map((projectData, key) => {
+            {projectList.map((projectData, key) => {
                 return <ProjectCard key={key} projectData={projectData}/>;
                 // key={`project-${projectData.id}`}
                 // projectData={projectData} - better way to write the above
